e11d6f72bfa74ed319dc32b71e3141cf1b0b4ba4
Madan AR <madanar@106AMBBLR-Nithin-A-Y.local>
2021-08-23 15:31:29 +0530

    read the input from the user and calculated the length of the line

--- LineCompare.java
+++ LineCompare.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
-
-public class LineCompare{
+public class UC1{
   public static void main(String args[])
   {
-    System.out.println("Welcome to line comaprison computation");
+	  
+  int x11,y11,x12,y12,x21,y21,x22,y22;
+  double distance1,distance2;
+  Scanner reader=new Scanner(System.in);
+  
+  System.out.println("Welcome to line comaprison computation");
+  System.out.println("Enter the coordinates of line 1");
+  
+  x11=reader.nextInt();
+  y11=reader.nextInt();
+  x12=reader.nextInt();
+  y12=reader.nextInt();
+  distance1=Math.sqrt((x12-x11)*(x12-x11)+(y12-y11)*(y12-y11));
+  
+  System.out.println("Enter the coordinates of line 2");
+  x21=reader.nextInt();
+  y21=reader.nextInt();
+  x22=reader.nextInt();
+  y22=reader.nextInt();
+  distance2=Math.sqrt((x22-x21)*(x22-x21)+(y22-y21)*(y22-y21));
+  
   }
 }
