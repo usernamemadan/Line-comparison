aaa30735225977091be68192ae362417d40bb755
madan <madan@local.com>
2021-08-31 23:21:57 +0530

    modified the programs according to OOPs concept

--- LineCompare.java
+++ LineCompare.java
@@ -1,44 +1,51 @@
 import java.util.Scanner;
 
-public class UC3{
-  public static void main(String args[])
-  {
-	  
-  int x11,y11,x12,y12,x21,y21,x22,y22;
-  double distance1,distance2;
-  Scanner reader=new Scanner(System.in);
-  
-  System.out.println("Welcome to line comaprison computation");
-  System.out.println("Enter the coordinates of line 1");
-  
-  x11=reader.nextInt();
-  y11=reader.nextInt();
-  x12=reader.nextInt();
-  y12=reader.nextInt();
-  
-  distance1=Math.sqrt((x12-x11)*(x12-x11)+(y12-y11)*(y12-y11));
-  
-  System.out.println("Enter the coordinates of line 2");
-  x21=reader.nextInt();
-  y21=reader.nextInt();
-  x22=reader.nextInt();
-  y22=reader.nextInt();
-  
-  distance2=Math.sqrt((x22-x21)*(x22-x21)+(y22-y21)*(y22-y21));
-  
-  
-  if((String.valueOf(distance1)).compareTo(String.valueOf(distance2))==0)
-		{
-			System.out.println("Two lines are equal");
-		}
-  else if((String.valueOf(distance1)).compareTo(String.valueOf(distance2))>0) {
-       System.out.println("Line1 is greater than Line2");
-    }
-	else{
-			System.out.println("Line1 is lesser than Line2");
+public class LineCompare {
+
+	int x11 = 0, y11 = 0, x12 = 0, y12 = 0, x21 = 0, y21 = 0, x22 = 0, y22 = 0;
+	double distance1 = 0, distance2 = 0;
+
+	LineCompare() {
+		System.out.println("Welcome to line comaprison computation");
 	}
 
-  System.out.println("The length of the line1 is:"+String.format("%.4f",distance1));
-  System.out.println("The length of the line2 is:"+String.format("%.4f",distance2));
-  }
+	public void input() {
+		System.out.println("Enter the coordinates of line 1");
+		Scanner sc = new Scanner(System.in);
+		x11 = sc.nextInt();
+		y11 = sc.nextInt();
+		x12 = sc.nextInt();
+		y12 = sc.nextInt();
+		System.out.println("Enter the coordinates of line 2");
+		x21 = sc.nextInt();
+		y21 = sc.nextInt();
+		x22 = sc.nextInt();
+		y22 = sc.nextInt();
+	}
+	public void findLength() {
+		distance1 = Math.sqrt((x12 - x11) * (x12 - x11) + (y12 - y11) * (y12 - y11));
+		distance2 = Math.sqrt((x22 - x21) * (x22 - x21) + (y22 - y21) * (y22 - y21));
+
+		System.out.println("The length of the line1 is:" + String.format("%.4f", distance1));
+		System.out.println("The length of the line2 is:" + String.format("%.4f", distance2));
+	}
+
+	public void isEqual() {
+		if ((String.valueOf(distance1)).compareTo(String.valueOf(distance2)) == 0) {
+			System.out.println("Two lines are equal");
+		}
+	}
+
+	public void compareLines() {
+		if ((String.valueOf(distance1)).compareTo(String.valueOf(distance2)) == 0) {
+			System.out.println("Two lines are equal");
+		} 
+		else if ((String.valueOf(distance1)).compareTo(String.valueOf(distance2)) > 0) {
+			System.out.println("Line1 is greater than Line2");
+		} 
+		else {
+			System.out.println("Line1 is lesser than Line2");
+		}
+	}
+
 }
---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[] args) {
+		LineCompare line = new LineCompare();
+		line.input();
+		line.findLength();
+		line.isEqual();
+		line.compareLines();
+	}
+}
